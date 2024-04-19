8c319cafcc7677f1bdecaa64ff4754a66a6a2245
Flooooooooooorian <florian.florianweber.weber@googlemail.com>
2024-04-19 14:52:01 +0200

    Add CI

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,4 +1,10 @@
+
 public class FizzBuzz {
+
+    public static void main(String[] args) {
+        System.out.println(fizzbuzz(99));
+        System.out.println("Hallo vom Terminal!");
+    }
 
     public static String fizzbuzz(int value) {
         if (value % 3 == 0 && value % 5 == 0) {
